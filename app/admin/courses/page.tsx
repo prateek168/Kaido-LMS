a27a4635368cc8f04bebf2a48a18f376a378dc41
Prateek Verma <prateekverma1681998@gmail.com>
2025-08-25 23:40:22 +0530

    feat: add course page

---
+++ app/admin/courses/page.tsx
@@ -0,0 +1,13 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+        <Link className={buttonVariants()} href="/admin/courses/create">Create Course</Link>
+      </div>
+    </>
+  );
+}
