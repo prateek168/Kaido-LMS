f267424d0398d1dc8ccadaa4873fb5805c5b706d
Prateek Verma <prateekverma1681998@gmail.com>
2025-12-17 22:29:11 +0530

    feat: update navbar

--- app/(public)/_components/Navbar.tsx
+++ app/(public)/_components/Navbar.tsx
@@ -42,7 +42,6 @@ const navigationItems: navigationItemsProps[] = [
 
 export function Navbar() {
   const { data: session, isPending } = authClient.useSession();
-  console.log(session?.user);
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 md:px-6">
--- app/(public)/_components/UserDropdown.tsx
+++ app/(public)/_components/UserDropdown.tsx
@@ -46,7 +46,7 @@ export function UserDropdown({ email, image, name }: iAppProps) {
           />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="max-w-64">
+      <DropdownMenuContent align="end" className="min-w-48">
         <DropdownMenuLabel className="flex min-w-0 flex-col">
           <span className="text-foreground truncate text-sm font-medium">
             {name}
